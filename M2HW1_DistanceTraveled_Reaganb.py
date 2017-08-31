1ed53d90ac970a9acd324be1cd3d6911b413f553
ProgramNoona <31457758+ProgramNoona@users.noreply.github.com>
2017-08-31 19:04:24 -0400

    Add files via upload
    Distance traveled program for class.

---
+++ M2HW1_DistanceTraveled_Reaganb.py
@@ -0,0 +1,21 @@
+# CTI-110
+# M2HW1 - Distance Traveled 
+# Bethany Reagan
+# August 29, 2017
+#
+# Program calculates the distance travelled of a car going 70 mph.
+
+print('A car is travelling at 70 miles per hour.\n')
+
+speed = 70
+hours6 = 6
+hours10 = 10
+hours15 = 15
+
+distanceAfter6 = hours6 * speed
+distanceAfter10 = hours10 * speed
+distanceAfter15 = hours15 * speed
+
+print('After six hours, the car has traveled', distanceAfter6, 'miles.')
+print('After ten hours, the car has traveled', distanceAfter10, 'miles.')
+print('After fifteen hours, the car has traveled', distanceAfter15, 'miles.')
