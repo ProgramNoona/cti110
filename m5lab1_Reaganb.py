ae0923eee19ef29a8e138d584cd01471fe17a31c
ProgramNoona <31457758+ProgramNoona@users.noreply.github.com>
2017-10-17 14:50:43 -0400

    Add files via upload
    These are programs to show how the turtle function in python works.

---
+++ m5lab1_Reaganb.py
@@ -0,0 +1,25 @@
+# Bethany Reagan
+# CTI 110
+# October 12, 2107
+# This program uses turtle to draw shapes
+
+import turtle
+turtle.shape("turtle")
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+
+turtle.right(90)
+turtle.forward(60)
+turtle.right(120)
+turtle.forward(50)
+turtle.right(120)
+turtle.forward(50)
+
+
+turtle.exitonclick
---
+++ m5lab2_Reaganb.py
@@ -0,0 +1,48 @@
+# Bethany Reagan
+# CTI 110
+# October 12, 2107
+# This program uses turtle to draw my initials
+
+import turtle
+turtle.shape("turtle")
+turtle.colormode(255)
+turtle.color(255, 105, 180)
+turtle.width(10)
+
+turtle.left(90)
+turtle.forward(110)
+turtle.right(125)
+turtle.forward(30)
+turtle.right(55)
+turtle.forward(25)
+turtle.right(55)
+turtle.forward(30)
+
+turtle.left(125)
+turtle.forward(45)
+turtle.right(70)
+turtle.forward(20)
+turtle.right(70)
+turtle.forward(45)
+
+turtle.left(160)
+turtle.penup()
+
+turtle.forward(50)
+turtle.left(90)
+turtle.pendown()
+
+turtle.forward(110)
+turtle.right(125)
+turtle.forward(30)
+turtle.right(55)
+turtle.forward(25)
+turtle.right(55)
+turtle.forward(30)
+
+turtle.left(100)
+turtle.forward(60)
+
+
+
+turtle.exitonclick
